8da10cdfb8b517b3c329db5f058b3ccff7be83a0
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-08-06 00:18:21 +0300

    update

--- src/Anonym/Components/Filesystem.php
+++ src/Anonym/Components/Filesystem.php
@@ -16,4 +16,65 @@
     class Filesystem
     {
 
+        /**
+         * @var DriverInterface
+         */
+        private $driver;
+
+
+        /**
+         * @var array
+         */
+        private $driverList;
+
+        /**
+         * Sınıfı başlatır ve seçilen driver ı ayarlar
+         *
+         * @param string $driver
+         */
+        public function __construct($driver = '')
+        {
+
+        }
+
+        /**
+         * @return DriverInterface
+         */
+        public function getDriver()
+        {
+            return $this->driver;
+        }
+
+        /**
+         * @param DriverInterface $driver
+         * @return Filesystem
+         */
+        public function setDriver($driver)
+        {
+            $this->driver = $driver;
+
+            return $this;
+        }
+
+        /**
+         * @return array
+         */
+        public function getDriverList()
+        {
+            return $this->driverList;
+        }
+
+        /**
+         * @param array $driverList
+         * @return Filesystem
+         */
+        public function setDriverList($driverList)
+        {
+            $this->driverList = $driverList;
+
+            return $this;
+        }
+
+
+
     }
