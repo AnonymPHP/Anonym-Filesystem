1ff18eb5c8291ebb3f36f3001414e59bf1610dc0
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-08-06 00:18:44 +0300

    added driver interface

---
+++ src/Anonym/Components/DriverInterface.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Filesystem;
+
+    /**
+     * Class DriverInterface
+     * @package Anonym\Components\Filesystem
+     */
+    class DriverInterface
+    {
+
+    }
