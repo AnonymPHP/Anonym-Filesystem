98dd048733fd37043d7c652ef2ddfc816ab46987
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-08-06 00:17:00 +0300

    added dirs and filesystem class

---
+++ src/Anonym/Components/Filesystem.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Filesystem;
+
+    /**
+     * Class Filesystem
+     * @package Anonym\Components\Filesystem
+     */
+    class Filesystem
+    W{
+
+    }
